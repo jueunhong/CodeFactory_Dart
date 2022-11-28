d6eecabc1a8fd7949fd87938e7f9a2775ea5acac
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-11-29 00:23:02 +0900

    Set

--- dart_application_1/bin/main.dart
+++ dart_application_1/bin/main.dart
@@ -1,45 +1,21 @@
 void main() {
-  //Map
-  //Key / Value 짝을 이뤄서 값
-  Map<String, String> dictionary = {
-    'Harry Potter': '해리포터',
-    'Ron Weasley': '론 위즐리',
-    'Hermione Granger': '헤르미온느 그레인저'
-  };
+  //Set
+  //중복값 하나로 처리
+  final Set<String> name = {'Code Factory', 'Flutter', 'Black Pink'};
 
-  print(dictionary);
+  print(name);
 
-  Map<String, bool> isHarryPotter = {
-    'Harry Potter': true,
-    'Ron Weasley': true,
-    'Ironman': false
-  };
+  name.add('Flutter');
 
-  print(isHarryPotter);
+  print(name);
 
-  //추가
-  isHarryPotter.addAll({'Spiderman': false});
+  name.remove('Flutter');
 
-  isHarryPotter['Hulk'] = false;
+  print(name);
 
-  print(isHarryPotter);
+  //값이 있는지 확인
+  print(name.contains('Code Factory'));
 
-  //value값 가져오기
-  print(isHarryPotter['Ironman']);
-
-  //value값 바꾸기
-
-  isHarryPotter['Spiderman'] = true;
-
-  print(isHarryPotter);
-
-  //삭제
-
-  isHarryPotter.remove('Harry Potter');
-
-  print(isHarryPotter);
-
-  //key 값만 , value값만 따로 가져오기
-  print(isHarryPotter.keys);
-  print(isHarryPotter.values);
+  List<String> blackPink = ['지수'];
+  print(blackPink.contains('지수'));
 }
--- summary.md
+++ summary.md
@@ -414,4 +414,30 @@ void main() {
 
 ```
 
+6. Set
+
+```dart
+void main() {
+  //Set
+  //중복값 하나로 처리
+  final Set<String> name = {'Code Factory', 'Flutter', 'Black Pink'};
+
+  print(name);
+
+  name.add('Flutter');
+
+  print(name);
+
+  name.remove('Flutter');
+
+  print(name);
+
+  //값이 있는지 확인
+  print(name.contains('Code Factory'));
+
+  List<String> blackPink = ['지수'];
+  print(blackPink.contains('지수'));
+}
+```
+
 
